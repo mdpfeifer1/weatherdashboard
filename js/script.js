eb85594989381081c5e84408bad79720aff4f012
mdpfeifer1 <mikelaidenpfeifer@gmail.com>
2023-09-29 20:59:58 -0500

    added url link in js, fetch and response js, added header and syles to html and css.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 // openweathermap api key 875e03e498e7f171d7376399275a0e94
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=875e03e498e7f171d7376399275a0e94
 
-// 
-// 
-// 
-// 
+ var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=875e03e498e7f171d7376399275a0e94'
+
+ fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('Fetch Response \n-------------');
+    console.log(data);
+  });
